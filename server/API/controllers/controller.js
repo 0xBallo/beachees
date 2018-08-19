094e0597e08eeadb7758c0cdd6ee27fa58866de3
mballo <ballomattia.ra@gmail.com>
2018-08-19 15:43:40 +0200

    update method

--- server/API/controllers/controller.js
+++ server/API/controllers/controller.js
@@ -1,11 +1,18 @@
 'use strict';
 const url = require('url');
-const Datastore = require('nedb');
+const Moment = require('moment');
 
 exports.info = (req, res) => {
    res.send('Hello, welcome to root of APIv1');
 }
 
+/**
+ * Get data from DHT collection in DB
+ * 
+ * @param {Datastore} db 
+ * @param {*} req 
+ * @param {*} res 
+ */
 exports.get_temp = (db, req, res) => {
    const urlParts = url.parse(req.url, true);
    const parameters = urlParts.query;
@@ -13,14 +20,20 @@ exports.get_temp = (db, req, res) => {
    const endDate = parameters.end;
 
    if (startDate == undefined || endDate == undefined) {
+      // INFO: retrieve all data
       db.dht.find({}, function (err, data) {
          res.json({
             data: data
          });
       });
    } else {
-      //TODO: retrieve data from DHT table in db between start and end date
-      db.dht.find({}, function (err, data) {
+      // INFO: retrieve data between date
+      db.dht.find({
+         date: {
+            $lt: end,
+            $gt: start
+         }
+      }, function (err, data) {
          res.json({
             data: data
          });
