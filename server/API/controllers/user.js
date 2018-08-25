b9bc93263bc3c9f3240763e5b3778c669c809d04
mballo <ballomattia.ra@gmail.com>
2018-08-25 15:58:20 +0200

    start push notification server

--- server/API/controllers/user.js
+++ server/API/controllers/user.js
@@ -1,3 +1,26 @@
 'use strict';
 const url = require('url');
 const Moment = require('moment');
+
+exports.add_user_device = (req, res, db) => {
+   let data = req.body;
+
+   if (data.u === undefined || data.uid === undefined) {
+      res.send('501: Incomplete data in request!');
+   } else {
+      // TODO: salvare la chiave per poter inviare notifiche al dispositivo
+   }
+};
+
+exports.get_user_id = (req, res, db) => {
+   const urlParts = url.parse(req.url, true);
+   const parameters = urlParts.query;
+   const user = parameters.user;
+
+   if (user === undefined) {
+      res.send('501: Incomplete data in request!');
+   } else {
+      //TODO: recuperare uid dal database
+   }
+
+};
